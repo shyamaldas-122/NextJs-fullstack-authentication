ba02d9876a7ca5f71da2ca7c3befc9153e3e6882
Shyamal Das <shyamald934@gmail.com>
2024-01-14 16:08:41 +0530

    Env variable change Test !

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
-
 export default function VerifyEmailPage() {
 
     const [token, setToken] = useState("");
